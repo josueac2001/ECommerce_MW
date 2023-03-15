e4a648f2c37db4fb394ef3b5b91eb59c1a9e21a7
JOSU� ACOSTA LONDO�O <josueacosta261853@correo.itm.edu.co>
2023-03-14 23:52:59 -0500

    create a new view with search and buttons to enhance the view of country in Index.cshtml

--- ECommerce_MW/ECommerce_MW/DAL/Entities/Entity.cs
+++ ECommerce_MW/ECommerce_MW/DAL/Entities/Entity.cs
@@ -8,7 +8,9 @@ namespace ECommerce_MW.DAL.Entities
         [Key] //PRIMARY KEY
         [Required] //NOT NULL
         public Guid Id { get; set; }
+        [Display(Name = "Fecha de creación")]
         public DateTime? CreatedDate { get; set; }
+        [Display(Name = "Fecha de modificación")]
         public DateTime? ModifiedDate { get; set; }
         #endregion
     }
