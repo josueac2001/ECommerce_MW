283e58602ffb4efe2517673654eba551b94fc016
JOSU� ACOSTA LONDO�O <josueacosta261853@correo.itm.edu.co>
2023-03-15 13:08:31 -0500

    New categoryes table created

---
+++ ECommerce_MW/ECommerce_MW/DAL/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce_MW.DAL.Entities
+{
+    public class Category: Entity
+    {
+        #region Properties
+        [Display(Name = "Categoría")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe ser de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public String Name { get; set; }
+        #endregion
+
+    }
+}
