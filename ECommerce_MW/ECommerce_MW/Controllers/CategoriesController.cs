f9dc50a85ec5a5ffa5c1ad84e758e29652e99e96
JOSU� ACOSTA LONDO�O <josueacosta261853@correo.itm.edu.co>
2023-03-15 13:48:59 -0500

    Index action and view created

---
+++ ECommerce_MW/ECommerce_MW/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using ECommerce_MW.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce_MW.Controllers
+{
+    public class CategoriesController: Controller
+    {
+        private readonly DatabaseContext _context;
+        public CategoriesController(DatabaseContext context)
+        {
+            _context = context;
+        }
+        // GET: Categories
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categories.ToListAsync());
+        }
+
+
+    }
+}
